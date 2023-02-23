3df03464b7341b1aafcb80661defa3108be62423
Phuong Anh <phuonganhpham783@gmail.com>
2023-02-23 23:56:59 +0700

    fix rui do

--- Keywords/testJS/TestFirstJS.groovy
+++ Keywords/testJS/TestFirstJS.groovy
@@ -26,7 +26,6 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 
 import internal.GlobalVariable
-import net.sf.jasperreports.compilers.JavaScriptCallableThisDecorator
 
 public class TestFirstJS {
 
